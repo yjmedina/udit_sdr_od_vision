2283102655845118450ea0183b8aca7a1ce5a7c2
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-05-21 18:58:20 +0200

    update setup for UDIT computers

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This repository contains my solution for an object detection model for self-driv
 ## Setup
 TODO
 
+```sh
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+```
+
+```sh
+uv venv --python 3.10
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+.\.venv\Scripts\Activate.ps1
+```
--- ul/train.py
+++ ul/train.py
@@ -1,6 +1,12 @@
 from ultralytics import YOLO
+import os
 
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-# Train the model with MPS
-results = model.train(data="self_driving_yolo/dataset.yaml",  device="mps", cfg="ul/default.yml")
+PATH_TO_DATASET = "data\YOLO_sdr_data\dataset.yaml"
+
+if __name__ == '__main__':
+    path_to_dataset = os.path.abspath(PATH_TO_DATASET)
+    assert os.path.exists(path_to_dataset)
+    # Load a model
+    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+    # Train the model with MPS
+    results = model.train(data=path_to_dataset, cfg="ul/default.yml", device=0)
