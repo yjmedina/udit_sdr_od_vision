edba805f2dee083155242a2fddd544d40f0881f0
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-05-21 19:37:47 +0200

    add different presets for initial testing and cli

--- ul/train.py
+++ ul/train.py
@@ -1,12 +1,22 @@
 from ultralytics import YOLO
 import os
+from typer import Typer
+
+
+cli = Typer()
 
 PATH_TO_DATASET = "data\YOLO_sdr_data\dataset.yaml"
 
-if __name__ == '__main__':
-    path_to_dataset = os.path.abspath(PATH_TO_DATASET)
+
+@cli.command()
+def main(
+        confpath: str,
+        path_to_dataset: str = PATH_TO_DATASET,
+):
+    abs_path = os.path.abspath(path_to_dataset)
     assert os.path.exists(path_to_dataset)
-    # Load a model
     model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-    # Train the model with MPS
-    results = model.train(data=path_to_dataset, cfg="ul/default.yml", device=0)
+    results = model.train(data=abs_path, cfg=confpath, device=0)
+
+if __name__ == '__main__':
+    cli()
